c88bab8f9878e001e0db3c74e914febfcc3a9e5c
Jefferson Swartz <moonlit-05-idol@icloud.com>
2022-11-07 21:57:16 +0530

    feat: added tailwind and TS support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import postcss from './postcss.config.js';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit()],
-  define: {
-    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
-  }
-};
-
-export default config;
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    postcss,
+  },
+  plugins: [svelte({
+    /* plugin options */
+  })]
+})
